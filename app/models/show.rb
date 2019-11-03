e6de03484ae8e4135e514cbca58c6fea100ffa9f
Kyle Luke <th.oldschool@gmail.com>
2019-11-03 18:16:58 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
 
   def actors_list
     self.actors.map do |a|
-      a.full_name + " - " + a.show.name
+      a.full_name + " - " + a.characters.show.name
     end
   end
 end
