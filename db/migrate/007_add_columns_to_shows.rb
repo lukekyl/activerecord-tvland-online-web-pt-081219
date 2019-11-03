dcf212fe0c4c2247aa8325a0ba7aae4313dce28e
Kyle Luke <th.oldschool@gmail.com>
2019-11-03 18:19:18 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_columns_to_shows.rb
+++ db/migrate/007_add_columns_to_shows.rb
@@ -1,8 +1,9 @@
-class AddColumnToCharacters < ActiveRecord::Migration[5.1]
+class AddColumnToShows < ActiveRecord::Migration[5.1]
   #to make an additive change to a schema we create
   #a new migration, and then in the change method,
   #we make the change.
   def change
-    add_column :characters, :catchphrase, :string
+    add_column :shows, :day, :string
+    add_column :shows, :season, :string
   end
 end
