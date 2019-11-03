b3ab060918eef0ad9a1a804fb0236ad931e34305
Kyle Luke <th.oldschool@gmail.com>
2019-11-03 17:49:47 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,8 +1,9 @@
-class CreateActors < ActiveRecord::Migration[5.1]
+class CreateCharacters < ActiveRecord::Migration[5.1]
   def change
-    create_table :actors do |t|
-      t.string :first_name
-      t.string :last_name
+    create_table :characters do |t|
+      t.string :name
+      t.integer :actor_id
+      t.integer :show_id
     end
   end
 end
